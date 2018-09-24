0ccadff698cfa943e9e625f5b96c8711df5ee5c3
Paskuihernandez <paskuihernandez@gmail.com>
2018-09-24 18:49:08 +0200

    Mov Bola

---
+++ PdA2_Android/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public KeyCode up;
+    public KeyCode down;
+
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 0.3f;
+	}
+	
+	// Update is called once per frame
+	/*void Update () {
+        if ()
+        {
+            transform.Translate(0, 1, 0);
+        }
+        if ()
+        {
+            transform.Translate(0, -1, 0);
+        }
+    }*/
+}
